691a360b4020e86bdd7407b62d00ea7198f553fb
Kamindu Nanayakkara <Kamindu.Nanayakkara@ebuilder.com>
2016-11-08 00:03:13 +0530

    Adding test cases to module

---
+++ polling-app/test/api-test/test-routes.js
@@ -0,0 +1,59 @@
+var supertest = require('supertest');
+var app = require('../../app');
+var should = require("should");
+
+
+// UNIT test begin
+describe("SAMPLE unit test",function(){
+
+    // #1 should return home page
+     it("should return home page",function(done){
+
+        // calling home page api
+        supertest(app)
+        .get("/")
+        .expect("Content-type",/json/)
+        .expect(200) // THis is HTTP response
+        .end(function(err,res){
+            // HTTP status should be 200
+            res.status.should.equal(200);           
+            done();
+        });
+    });
+
+
+    it("should return poll page get",function(done){
+        supertest(app)
+        .get('/polls')
+        .expect(200)
+        .end(function(err,res){
+            // HTTP status should be 200
+            res.status.should.equal(200);               
+            done();
+        });
+    });
+
+    it("should return poll page post",function(done){
+        supertest(app)
+        .post('/polls')
+        .expect(200)
+        .end(function(err,res){
+            // HTTP status should be 200
+            res.status.should.equal(200);               
+            done();
+        });
+    });
+
+
+    it("other links should return 404",function(done){
+        supertest(app)
+        .get("/random")
+        .expect(404)
+        .end(function(err,res){
+            // HTTP status should be 404
+            res.status.should.equal(404);
+            done();
+        });
+    });
+
+});
