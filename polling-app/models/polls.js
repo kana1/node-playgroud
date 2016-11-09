7aaeee5bb3e298a5976635120af0145778950ff6
Kamindu Nanayakkara <Kamindu.Nanayakkara@ebuilder.com>
2016-11-09 07:11:24 +0530

    Adding polling function handler file

---
+++ polling-app/models/polls.js
@@ -0,0 +1,6 @@
+// this will be responsible to handle polling taskes as menstioned below 
+   // Creating new poll.
+   // Updating votes.
+   // Getting all polls.
+
+   // the bridge between app and db (DAO layer)
