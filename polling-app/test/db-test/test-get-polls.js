a5041f0d13b25788d18c6f5f4f40934ae014c556
kana1 <kamindu.nanayakkara@ebuilder.com>
2016-12-11 16:28:42 +0530

    initial adding DAO layer tests

---
+++ polling-app/test/db-test/test-get-polls.js
@@ -0,0 +1,13 @@
+
+
+ describe("DAO Get all polls", function(){
+         // Test will pass if we get all polls
+        xit("should return all todos", function(done){
+          
+        });
+
+        // Test will pass if we fail to get a poll
+        xit("should return error", function(done){
+            
+        });
+    });
---
+++ polling-app/test/db-test/test-post-poll.js
@@ -0,0 +1,16 @@
+var sinon = require('sinon');
+
+var mongoose = require('mongoose');
+require('sinon-mongoose');
+
+
+ // Test will pass if the poll is saved
+describe("DAO Post a new poll", function(){
+    xit("should create new post", function(done){
+        
+    });
+    // Test will pass if the poll is not saved
+    xit("should return error, if poll not saved", function(done){
+        
+    });
+});
