0dac0929059c2d0c8b46c356cb33454d2708be61
miraj <tmirajhamid@gmail.com>
2017-01-14 12:12:54 +0530

    Fix app crash after wrong data input

--- polling-app/controller/polls.js
+++ polling-app/controller/polls.js
@@ -27,6 +27,7 @@ router.route('/')
       // Code to add new polls.
       console.log("/post called")
       var pollObject = new pollModel();
+      var isvalidated = false;
       // Calling our model function.
       // make sure to set content-type header to content-type:application/json
       var data = {
@@ -34,26 +35,31 @@ router.route('/')
         options: req.body.polls
       }
       
-      
-      if(!(data.question === undefined )){
-    	  
+      //validate 
+      if(!(data.question === undefined ) || !(data.options === undefined )){
+    	  isvalidated = true;
     	  console.log(typeof req.body.polls)
           console.log(data)
       }else{
-    	  
+    	  isvalidated = false;
+    	  console.log(data);
     	  response = {"error" : true,"message" : "Invalid input data type"};
     	  res.json(response);
       }
       
-      
-       pollObject.addNewPolls(data, function (err){
-               if(err) {
-                    response = {"error" : true,"message" : "Error adding poll data"};
-               } else {
-                    response = {"error" : false,"message" : "Poll added"};
-               }
-               res.json(response);
-       });
+      if(isvalidated){
+    	  
+    	  pollObject.addNewPolls(data, function (err){
+              if(err) {
+                   response = {"error" : true,"message" : "Error adding poll data"};
+              } else {
+                   response = {"error" : false,"message" : "Poll added"};
+              }
+              res.json(response);
+    	  });
+    	  
+      }
+       
     	  
       
       
