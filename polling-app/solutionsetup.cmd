2e7b550353038524581e70e438fc656cb06fb50a
kana1 <kamindu.nanayakkara@ebuilder.com>
2017-07-16 22:01:14 +0530

    changing sript permission

--- polling-app/solutionsetup.cmd
+++ polling-app/solutionsetup.cmd
@@ -1,2 +1,4 @@
-powershell.exe -Command "& {set-executionpolicy remotesigned currentuser}"
-powershell.exe .\mongodb-mng.ps1
+REM powershell.exe -Command "& {set-executionpolicy remotesigned currentuser}"
+REM powershell.exe .\mongodb-mng.ps1
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '.\mongodb-mng.ps1'";
