cfbaa16cf237c281f8663f0f85d04cd565fc5bf9
kana1 <kamindu.nanayakkara@ebuilder.com>
2017-07-18 07:00:52 +0530

    test download

--- polling-app/test.cmd
+++ polling-app/test.cmd
@@ -4,6 +4,6 @@ REM $url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4
 REM $zipFile = "$mongoDbPath\mongo.zip" 
 REM $unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-2.4.9"
 
-mkdir "D:\home\MongoDB"
-REM curl -OutFile "mongo.zip" "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
-curl -o "test'zip" "http://www.colorado.edu/conflict/peace/download/peace.zip"
+REM mkdir "D:\home\MongoDB"
+curl -o "mongo.zip" "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
+REM curl -o "test.zip" "http://www.colorado.edu/conflict/peace/download/peace.zip"
