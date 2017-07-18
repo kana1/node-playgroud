bbc409fc09d4bf22ce1b7eec89d4fc504f3fac60
kana1 <kamindu.nanayakkara@ebuilder.com>
2017-07-18 06:56:51 +0530

    test download

--- polling-app/test.cmd
+++ polling-app/test.cmd
@@ -1,8 +1,8 @@
-$mongoDbPath = "D:\home\MongoDB" 
-$mongoDbConfigPath = "$mongoDbPath\mongod.cfg"
-$url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
-$zipFile = "$mongoDbPath\mongo.zip" 
-$unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-2.4.9"
+REM $mongoDbPath = "D:\home\MongoDB" 
+REM $mongoDbConfigPath = "$mongoDbPath\mongod.cfg"
+REM $url = "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
+REM $zipFile = "$mongoDbPath\mongo.zip" 
+REM $unzippedFolderContent ="$mongoDbPath\mongodb-win32-x86_64-2008plus-2.4.9"
 
 mkdir "D:\home\MongoDB"
 REM curl -OutFile "mongo.zip" "http://downloads.mongodb.org/win32/mongodb-win32-x86_64-2008plus-2.4.9.zip" 
